275d934963c89ad5549791d3aa2beef7605d95a2
sanjaymohan <sanjaykakkatiri@gmal.com>
2024-01-08 22:37:47 +0530

    portfolio website

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    // var x = document.getElementById("myNavbar");
+    // if (x.className === "navbar") {
+    //   x.className += " responsive";
+    // } else {
+    //   x.className = "navbar";
+    // }
+    var overlay = document.getElementById("myOverlay");
+    if (overlay.style.display === "none") {
+        overlay.style.display = "flex";
+    } else {
+        overlay.style.display = "none";
+    }
+  }
+
+  // Close button functionality
+  function closeMenu() {
+    
+}
